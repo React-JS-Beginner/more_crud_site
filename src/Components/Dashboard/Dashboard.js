6effbbd0875c061fd592140de9e046c4419dc732
avishekataur <avishekataur@gmail.com>
2022-01-01 23:41:22 +0600

    done-for-today-2022

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
     <>
       <div className="dashboard pt-4">
-        <span className="text-uppercase">
-          DashBoard
-        </span>
+        <span className="text-uppercase">DashBoard</span>
       </div>
-      <hr class="new" />
+      <hr className="new" />
+      <Container className="mt-2">
+        <Row>
+          <Col xl={2} lg={2} className="side-menu">
+            <p className="text-center pt-3">Side Menu</p>
+          </Col>
+          <Col xl={10} lg={10}>
+            <p className="text-center pt-3">Product Orders</p>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
